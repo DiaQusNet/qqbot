96524310e1331576bdc50c899c7d053ae02a80cb
DiaQusNet <98730077+DiaQusNet@users.noreply.github.com>
2024-08-18 12:57:22 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from botpy import logging
 from botpy.ext.cog_yaml import read
 from botpy.message import GroupMessage, Message
 
-from features.ollama_replay import messages_with_id
+from features.ollama_replay import messages_with_id, clean_memory, reply
 
 test_config = read(os.path.join(os.path.dirname(__file__), "config.yaml"))
 
@@ -17,15 +17,25 @@ class MyClient(botpy.Client):
         _log.info(f"robot 「{self.robot.name}」 on_ready!")
 
     async def on_group_at_message_create(self, message: GroupMessage):
-        message_list = messages_with_id(message.author.member_openid, message.group_openid, message.content)
-        ai_message = reply(message_list)
-        messages_with_id(message.author.member_openid, message.group_openid, ai_message, True)
-        messageResult = await message._api.post_group_message(
-            group_openid=message.group_openid,
-            msg_type=0, 
-            msg_id=message.id,
-            content=ai_message)
-        _log.info(messageResult)
+        if '/记忆清除' in message.content:
+            clean_memory()
+            messageResult = await message._api.post_group_message(
+                group_openid=message.group_openid,
+                msg_type=0, 
+                msg_id=message.id,
+                content='记忆被清除了...')
+            _log.info(messageResult)
+
+        else:
+            message_list = messages_with_id(message.author.member_openid, message.group_openid, message.content)
+            ai_message = reply(message_list)
+            messages_with_id(message.author.member_openid, message.group_openid, ai_message, True)
+            messageResult = await message._api.post_group_message(
+                group_openid=message.group_openid,
+                msg_type=0, 
+                msg_id=message.id,
+                content=ai_message)
+            _log.info(messageResult)
 
 
 if __name__ == "__main__":
